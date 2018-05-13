362e3aa1b20d9d400d37c95cd7b0d672d1504e17
Ricky <rickylford@users.noreply.github.com>
2018-05-13 00:13:42 -0600

    Update check-vpn-speeds.sh
    Added the log file implementation throughout the script.

--- check-vpn-speeds.sh
+++ check-vpn-speeds.sh
@@ -13,7 +13,7 @@ DIR="/etc/openvpn"
 EXT=".conf"
 
 # LOCATION OF THE SPEEDTEST-CLI EXECUTABLE
-SCLI="/etc/openvpn"
+SCLI="/data/Scripts"
 
 # SECONDS TO WAIT UNTIL VPN CONNECTIONS TIMEOUT AND MOVE ON
 TIMEOUT=10
@@ -25,6 +25,9 @@ TIMEOUT=10
 # THAT ARE GOING TO TAKE FOREVER TO COMPLETE.
 PINGTHRESHOLD=100
 
+# LOG FILE LOCATION WITH FULL PATH
+LOGLOCATION="/var/log/check-vpn-speeds.log"
+
 ##################################################################
 # DO NOT EDIT BELOW THIS LINE
 ##################################################################
@@ -34,20 +37,23 @@ BESTSPEEDPROVIDER=""
 
 # STOP OPENVPN, KILLING ANY CURRENT CONNECTION
 service_stop() {
-	# CHECK TO SEE IF THE OPENVPN PROCESS IS RUNNING, AND IF IT IS, STOP IT, IF NOT, DO NOTHING
-        #systemctl stop openvpn
-
-	# CHECK IF THE OPENVPN PROCESS IS RUNNING; IF SO, END IT
+	echo "[ $(date) ]: service_stop() FUNCTION STARTED" >> $LOGLOCATION
 
         # SLEEP FOR 1 SECOND INDEFINITELY UNTIL THE OPENVPN SERVICE IS STOPPED
         while [ "0" != `sudo ifconfig | grep tun0 | wc -l` ]; do
+		echo "[ $(date) ]: STOPPING OPENVPN SYSTEMCTL" >> $LOGLOCATION
 		systemctl stop openvpn
                 sleep 1
         done
+
+	echo "[ $(date) ]: service_stop() FUNCTION COMPLETED" >> $LOGLOCATION
 }
 
 # STARTS OPENVPN WITH A SPECIFIC CONFIGURATION FILE
 service_start() {
+	echo "[ $(date) ]: service_start() FUNCTION STARTED" >> $LOGLOCATION
+
+	echo "[ $(date) ]: STARTING OPENVPN SERVICE WITH $1" >> $LOGLOCATION
         systemctl start openvpn@$1.service
 
 	# THIS SECTION WILL FIRST SET A COUNTER TO 0, AT WHICH POINT THE SCRIPT
@@ -59,12 +65,15 @@ service_start() {
                 sleep 1
 		COUNTER=$((COUNTER+1))
 
-		# IF OUR COUNTER INCREMENT GOES ABOVE OUT TIMEOUT LIMIT, BREAK OUT
+		# IF OUR COUNTER INCREMENT GOES ABOVE OUR TIMEOUT LIMIT, BREAK OUT
 		if [[ $COUNTER -gt $TIMEOUT ]]
 		then
+			echo "[ $(date) ]: service_start() COUNTER MAXED OUT; BREAKING FROM FUNCTION" >> $LOGLOCATION
 			break
 		fi
         done
+
+	echo "[ $(date) ]: service_start() FUNCTION COMPLETED" >> $LOGLOCATION
 }
 
 # THIS IS WHERE WE CONDUCT THE SPEED TEST, AND SET THE BESTSPEED/BESTSPEEDPROVIDER ACCORDINGLY
@@ -73,19 +82,26 @@ speed_test() {
 	# SETTINGS, WE WANT TO MAKE SURE WE ARE CONNECTED TO THE VPN. OTHERWISE, THE SPEEDTEST
 	# WILL BE OUR OWN PROVIDER, AND THE BESTSPEEDPROVIDER WILL BE THE ONE THAT FAILED
 	while [ "0" == `sudo ifconfig | grep tun0 | wc -l` ]; do
+		echo "[ $(date) ]: CONNECTION TO $1 VPN FAILED. SKIPPING IT." >> $LOGLOCATION
+
 		echo "VPN CONNECTION FAILED. SKIPPING."
 		return 0
         done
 
         IPCITY=$(curl -s ipinfo.io/city)
         IPADDRESS=$(curl -s ipinfo.io/ip)
+
         echo "INFO: $IPCITY / $IPADDRESS"
+	echo "[ $(date) ]: $1 INFO: $IPCITY / $IPADDRESS" >> $LOGLOCATION
 
         TESTRESULT=$($SCLI/speedtest-cli --no-upload --no-pre-allocate | grep Download | cut -d ' ' -f 2 | cut -d '.' -f 1)
+
         echo "DOWNLOAD: $TESTRESULT MB/s"
+	echo "[ $(date) ]: $1 SPEEDTEST RESULT: $TESTRESULT MB/s" >> $LOGLOCATION
 
         if [[ $TESTRESULT -gt $BESTSPEED ]]
         then
+		echo "[ $(date) ]: $1 DETECTED AS BEST RESULT SO FAR" >> $LOGLOCATION
                 BESTSPEED=$TESTRESULT
                 BESTSPEEDPROVIDER=$1
         fi
@@ -93,6 +109,8 @@ speed_test() {
 
 # RUN THROUGH THE LIST OF VPN CONNECTION FILES IN THE OPENVPN DIRECTORY
 TOTALFILES=$(ls -1q "$DIR"/*"$EXT" | wc -l)
+echo "[ $(date) ]: FOUND $TOTALFILES $EXT FILES IN $DIR" >> $LOGLOCATION
+
 FILECOUNT=1
 for file in "$DIR"/*"$EXT"
 do
@@ -100,39 +118,53 @@ do
         file=${file%$EXT} # REMOVE THE EXTENSION FROM THE FILE NAME
 
         # FIRST, WE SHOULD STOP OPENVPN JUST TO MAKE SURE WE DON'T HAVE AN ACTIVE CONNECTION
+	echo "[ $(date) ]: CALLING SERVICE_STOP FUNCTION" >> $LOGLOCATION
         service_stop
 
 	echo "FILE: ${file}$EXT ($FILECOUNT of $TOTALFILES)"
 
-	URLTOTEST=$(awk '/^remote /{print $2}' ${file}$EXT)
+	URLTOTEST=$(awk '/^remote /{print $2}' $DIR/${file}$EXT)
 	PINGRESPONSEFLOAT=$(ping -c 4 $URLTOTEST | tail -1| awk '{print $4}' | cut -d '/' -f 2)
 	PINGRESPONSE=${PINGRESPONSEFLOAT%.*}
+	echo "[ $(date) ]: GETTING $PINGRESPONSEFLOAT AVERAGE PING TO $URLTOTEST" >> $LOGLOCATION
 
 	if [[ $PINGRESPONSE -lt $PINGTHRESHOLD ]]
 	then
+		echo "[ $(date) ]: PING RESPONSE IS WITHIN THRESHOLD" >> $LOGLOCATION
+
 	        # START THE SERVICE
+		echo "[ $(date) ]: FUNCTION CALL : service_start ${file}" >> $LOGLOCATION
 	        service_start ${file}
 
 	        # CONDUCT THE SPEED TEST
+		echo "[ $(date) ]: FUNCTION CALL : speed_test ${file}" >> $LOGLOCATION
 	        speed_test ${file}
 
 	        # STOP THE SERVICE SO THAT WE CAN MOVE ON
+		echo "[ $(date) ]: FUNCTION CALL : service_stop" >> $LOGLOCATION
 	        service_stop
 
 	        echo ""
 	else
+		echo "[ $(date) ]: PING RESPONSE WAS TOO HIGH; SKIPPING THIS $EXT FILE." >> $LOGLOCATION
+
 		echo "PING RESPONSE TOO HIGH ($PINGRESPONSEFLOAT). SKIPPING."
 		echo ""
 	fi
+
 	FILECOUNT=$((FILECOUNT+1))
 done
 
 # IF WE CAPTURED A BEST SPEED AT ONE POINT, LET'S CONTINUE TO THE NEXT CHECK
 if [[ $BESTSPEED -gt 0 ]]
 then
+	echo "[ $(date) ]: WE HAVE A HIGH SPEED BETTER THAN 0" >> $LOGLOCATION
+
         # SINCE WE HAVE A BEST SPEED, DO WE HAVE AN ACTUAL PROVIDER?
         if [ $BESTSPEEDPROVIDER != "" ]
         then
+		echo "[ $(date) ]: SETTING VPN CONNECTION TO $BESTSPEEDPROVIDER" >> $LOGLOCATION
+
                 echo "CONNECTING TO: $BESTSPEEDPROVIDER"
                 systemctl start openvpn@$BESTSPEEDPROVIDER.service
         fi
